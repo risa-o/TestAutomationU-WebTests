b0ff2e41c73989a1da98a9fd296826752e1307f7
risa-o <risa.orsinger@gmail.com>
2019-10-07 16:02:17 -0500

    Test Automation University Selenium Tests with Java 4.2

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,34 +5,29 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
+        homePage = new HomePage(driver);
+    }
 
-        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContentLink.click();
-
-        driver.findElement(By.linkText("Example 1: Menu Element")).click();
-
-        List<WebElement> links = driver.findElements(By.tagName("li"));
-        System.out.println(links.size());
-
-
-        System.out.println(driver.getTitle());
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
