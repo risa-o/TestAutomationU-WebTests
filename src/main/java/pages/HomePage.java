371431da615e0f713235d81ab43fdfcc56d18e5e
risa-o <risa.orsinger@gmail.com>
2019-10-07 16:50:14 -0500

    Test Automation University Selenium Tests with Java 4.3

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,13 +6,22 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
 
     private WebDriver driver;
-    private By formAuthenticationLink = By.linkText("Form Authentication");
+
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
 
     public LoginPage clickFormAuthentication(){
-        driver.findElement(formAuthenticationLink).click();
+        clickLink("FormAuthentication");
         return new LoginPage(driver);
+    }
+
+    public DropdownPage clickDropDown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+    private void clickLink(String linktext) {
+        driver.findElement(By.linkText(linktext)).click();
     }
 }
